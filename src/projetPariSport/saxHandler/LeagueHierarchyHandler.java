499629761e39b1ef09a18e5ac54a4f033e2a565d
Rodier Madiande <r.madiande@gmail.fr>
2013-12-26 23:55:51 +0100

    Adding LeagueHierarchy's object

---
+++ src/projetPariSport/saxHandler/LeagueHierarchyHandler.java
@@ -0,0 +1,92 @@
+package projetPariSport.saxHandler;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import projetPariSport.structObject.LeagueHierarchy;
+
+public class LeagueHierarchyHandler extends DefaultHandler {
+	private List<LeagueHierarchy> leagueHierarchies;
+	private LeagueHierarchy leagueHierarchy;
+	private String leagueId, leagueName, leagueAlias,
+	conferenceId, conferenceName, conferenceAlias, divisionId, divisionName, divisionAlias;
+	private StringBuffer buffer;
+	
+	public LeagueHierarchyHandler() {
+		// TODO Auto-generated constructor stub
+		leagueHierarchies = new  LinkedList<LeagueHierarchy>();
+	}
+	
+	public void startElement(String uri, String localName,
+			String qName, Attributes attributes) throws SAXException{
+		
+		if(qName.equals("league")){
+			leagueId = attributes.getValue("id");
+			leagueName = attributes.getValue("name");
+			leagueAlias = attributes.getValue("alias");
+		}else if(qName.equals("conference")){
+			conferenceId = attributes.getValue("id");
+			conferenceName = attributes.getValue("name");
+			conferenceAlias = attributes.getValue("alias");
+		}
+		else if(qName.equals("division")){
+			divisionId = attributes.getValue("id");
+			divisionName = attributes.getValue("name");
+			divisionAlias = attributes.getValue("alias");
+		}else if(qName.equals("team")){
+			leagueHierarchy = new LeagueHierarchy();
+			leagueHierarchy.setLeagueId(leagueId);
+			leagueHierarchy.setLeagueName(leagueName);
+			leagueHierarchy.setLeagueAlias(leagueAlias);
+			leagueHierarchy.setConferenceId(conferenceId);
+			leagueHierarchy.setConferenceName(conferenceName);
+			leagueHierarchy.setConferenceAlias(conferenceAlias);
+			leagueHierarchy.setDivisionId(divisionId);
+			leagueHierarchy.setDivisionName(divisionName);
+			leagueHierarchy.setDivisionAlias(divisionAlias);
+			leagueHierarchy.setAttributesValues(attributes, qName);
+		}
+		else if(qName.equals("venue")){
+			leagueHierarchy.setAttributesValues(attributes, qName);
+		}
+		else{
+			/**/
+		}
+	}
+	
+	public void endElement(String uri, String localName, String qName){
+		if(qName.equals("team")){
+			leagueHierarchies.add(leagueHierarchy);
+			buffer = null;
+			leagueHierarchy = null;
+		}
+		else{
+			/*DO NOTHIN*/
+			buffer = null;
+		}
+	}
+	
+	public void characters(char[] ch, int start, int length) throws SAXException{
+		String lecture = new String(ch, start, length);
+		if(buffer != null) buffer.append(lecture);
+	}
+	
+	public void startDocument() throws SAXException{
+		System.out.println("Beginning of parsing:");
+	}
+	
+	public void endDocument() throws SAXException{
+		for(LeagueHierarchy b : leagueHierarchies){
+			System.out.println(b.toString());
+		}
+	}
+	
+	public List<LeagueHierarchy> getLeagueHierarchies()
+	{
+		return leagueHierarchies;
+	}
+}
